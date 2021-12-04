1183d1355f335a88a0f051035b9e36f748227f4c
rohansinghwow <rohansinghwow@gmail.com>
2021-12-04 21:40:37 +0530

    Configer react cdn

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
+
+import CardTop from './components/CardTop'
+import CardBtn from './components/CardBtn'
+import CardDetails from './components/CardDetails'
+import CardFooter from './components/CardFooter'
+
+
 export default function Card(){
 
 	return(
 
 		<div className="card">
-			
+
+			<CardTop/>
+			<CardBtn/>
+			<CardDetails/>
+			<CardFooter/>
+
 		</div>
 
 		)
 }
+
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Card from './components/Card.jsx'
 
+function Page(){
+	return (
+		
+			<Card/>
+		
+		)
+}
+
+ReactDOM.render(<Page/> , document.getElementById('root'))
